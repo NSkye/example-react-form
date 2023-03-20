71c0c288de6ef59e1dd06652181b3c0265b2b93d
Nikolai <nskye@pm.me>
2023-03-20 14:23:26 +0100

    Apply styles and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,75 @@
+import React, { useCallback } from 'react';
+
 import { Fields } from '@/components/Fields';
 
 import './App.css';
+import illustration from './assets/woman-tipping-hand.png';
 
 function App() {
+  const [currentValues, setCurrentValues] = React.useState<Record<string, string>>({});
+  const [isValid, setIsValid] = React.useState<boolean>(true);
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<unknown>) => {
+      e.preventDefault();
+      console.log(currentValues);
+    },
+    [currentValues],
+  );
+
+  const handleValid = useCallback((hasErrors: boolean) => {
+    setIsValid(!hasErrors);
+  }, []);
+
+  const handleChange = useCallback((values: Record<string, string>) => {
+    setCurrentValues(values);
+  }, []);
+
   return (
     <div className="App">
-      <Fields
-        fields={[
-          {
-            id: 'first_name',
-            type: 'inputText',
-            label: 'First Name',
-            defaultValue: 'Some first name',
-          },
-          {
-            id: 'last_name',
-            type: 'inputText',
-            label: 'Last Name',
-          },
-          {
-            id: 'email',
-            type: 'inputEmail',
-            label: 'Email',
-            required: true,
-          },
-          {
-            id: 'password',
-            type: 'inputPassword',
-            label: 'Password',
-            required: true,
-          },
-        ]}
-      />
+      <div aria-hidden className="App__illustration">
+        <img src={illustration} className="App__image" alt="illustration" />
+      </div>
+      <h2 className="App__heading">Авторизация</h2>
+      <form className="form" onSubmit={onSubmit}>
+        <Fields
+          onValid={handleValid}
+          description={
+            <p className="App__caption">
+              Для доступа к личному кабинету вашей компании авторизуйтесь на сайте.
+            </p>
+          }
+          onChange={handleChange}
+          fields={[
+            {
+              id: 'first_name',
+              type: 'inputText',
+              label: 'First Name',
+              defaultValue: 'Some first name',
+            },
+            {
+              id: 'last_name',
+              type: 'inputText',
+              label: 'Last Name',
+            },
+            {
+              id: 'email',
+              type: 'inputEmail',
+              label: 'Email',
+              required: true,
+            },
+            {
+              id: 'password',
+              type: 'inputPassword',
+              label: 'Password',
+              required: true,
+            },
+          ]}
+        />
+        <button className="form__submit" onSubmit={onSubmit} type="submit" disabled={!isValid}>
+          Войти
+        </button>
+      </form>
     </div>
   );
 }
--- src/components/Fields/Fields.tsx
+++ src/components/Fields/Fields.tsx
@@ -1,9 +1,15 @@
-import { useId } from 'react';
+import { useCallback, useId, useMemo } from 'react';
 
-import { Field, Form, Watch, toSubscribtionName } from '@libs/observable-form';
+import { Field, Form, Watch } from '@libs/observable-form';
 
+import { ObservableFormState } from '@/libs/observable-form/core';
+
+import './Fields.css';
 import { FieldConfig, FieldsComponent } from './Fields.types';
 import { validate } from './helpers';
+
+const VALUES: ['values'] = ['values'];
+const ERRORS: ['errors'] = ['errors'];
 
 const getInputType = (typeInConfig: FieldConfig<never>['type']) =>
   ({
@@ -12,51 +18,84 @@ const getInputType = (typeInConfig: FieldConfig<never>['type']) =>
     inputPassword: 'password',
   }[typeInConfig]);
 
-export const Fields: FieldsComponent = ({ fields }) => {
+export const Fields: FieldsComponent = ({ fields, onChange, onValid, description }) => {
   const id = useId();
-  const makeId = (fieldId: string) => `${id}-${fieldId}`;
-  const hintId = (fieldId: string) => `${makeId(fieldId)}-hint`;
+  const makeId = useCallback((fieldId: string) => `${id}-${fieldId}`, [id]);
+  const hintId = useCallback((fieldId: string) => `${makeId(fieldId)}-hint`, [makeId]);
 
-  const renderedFields = fields.map(field => {
-    const inputId = makeId(field.id);
-    const hintInputId = hintId(field.id);
+  const handleChange = useCallback(
+    (state: ObservableFormState) => {
+      onChange?.(state.values as Record<(typeof fields)[number]['id'], string>);
+    },
+    [onChange],
+  );
 
-    return (
-      <Field
-        key={field.id}
-        name={field.id}
-        initialValue={field.defaultValue}
-        validate={validate(field)}
-      >
-        {({ inputProps, meta }) => (
-          <div>
-            <label htmlFor={inputId}>{field.label}</label>
-            <input
-              type={getInputType(field.type)}
-              aria-describedby={hintInputId}
-              id={inputId}
-              {...inputProps}
-            />
-            <p role="alert" aria-live="assertive" aria-relevant="text" id={hintInputId}>
-              {meta.touched && !!meta.error && meta.error}
-            </p>
-          </div>
+  const handleErrors = useCallback(
+    (state: ObservableFormState) => {
+      const errors = state.errors;
+      const hasErrors = Object.values(errors).some(error => !!error);
+      onValid?.(hasErrors);
+    },
+    [onValid],
+  );
+
+  const renderedFields = useMemo(
+    () =>
+      fields.map(field => {
+        const inputId = makeId(field.id);
+        const hintInputId = hintId(field.id);
+
+        return (
+          <Field
+            key={field.id}
+            name={field.id}
+            initialValue={field.defaultValue}
+            validate={validate(field)}
+          >
+            {({ inputProps, meta }) => (
+              <div className="field">
+                <div className="field__with-hint">
+                  <input
+                    className="field__input"
+                    type={getInputType(field.type)}
+                    required={field.required}
+                    placeholder={field.label}
+                    aria-describedby={hintInputId}
+                    aria-label={field.label}
+                    id={inputId}
+                    {...inputProps}
+                  />
+                  <p
+                    className="field__hint"
+                    role="alert"
+                    aria-live="assertive"
+                    aria-relevant="text"
+                    id={hintInputId}
+                  >
+                    {meta.touched && !!meta.error && meta.error}
+                  </p>
+                </div>
+              </div>
+            )}
+          </Field>
+        );
+      }),
+    [fields, hintId, makeId],
+  );
+
+  return useMemo(
+    () => (
+      <Form>
+        {() => (
+          <fieldset className="fields">
+            {description && <legend className="legend">{description}</legend>}
+            {renderedFields}
+            <Watch changes={VALUES} onChange={handleChange} />
+            <Watch changes={ERRORS} onChange={handleErrors} />
+          </fieldset>
         )}
-      </Field>
-    );
-  });
-
-  return (
-    <Form>
-      {() => (
-        <div>
-          {renderedFields}
-          <Watch
-            changes={fields.map(({ id }) => toSubscribtionName(id))}
-            onChange={state => console.log(state.values)}
-          />
-        </div>
-      )}
-    </Form>
+      </Form>
+    ),
+    [description, handleChange, handleErrors, renderedFields],
   );
 };
